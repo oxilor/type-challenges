0c9dac2be120936aff086df972966ff5103957ce
Ilya Ordin <oxilor@gmail.com>
2023-01-08 02:52:50 +0300

    Fix the solution for the challenge 274 so that all new test cases with big numbers pass

--- questions/00274-extreme-integers-comparator/solution.ts
+++ questions/00274-extreme-integers-comparator/solution.ts
@@ -9,12 +9,26 @@ type BuildTuple<N extends string, T extends any[] = []> =
     ? T
     : BuildTuple<N, [...T, 0]>
 
-type FirstIsGreaterThanSecond<A extends string, B extends string, T extends any[] = BuildTuple<A>> =
+type FirstDigitIsGreaterThanSecond<A extends string, B extends string, T extends any[] = BuildTuple<A>> =
   `${T['length']}` extends B
     ? true
     : T['length'] extends 0
       ? false
-      : FirstIsGreaterThanSecond<A, B, Pop<T>>
+      : FirstDigitIsGreaterThanSecond<A, B, Pop<T>>
+
+type FirstIsLongerThanSecond<A extends string, B extends string> =
+  FirstDigitIsGreaterThanSecond<`${GetLetters<A>['length']}`, `${GetLetters<B>['length']}`>
+
+type FirstIsGreaterThanSecond<A extends string, B extends string> =
+  A extends `${infer AF}${infer AR}`
+    ? B extends `${infer BF}${infer BR}`
+      ? AF extends BF
+        ? FirstIsGreaterThanSecond<AR, BR>
+        : FirstDigitIsGreaterThanSecond<AF, BF> extends true
+          ? true
+          : false
+      : never
+    : never
 
 type CompareNumsWithSameSign<A extends string, B extends string, IsPositive extends boolean> =
   FirstIsGreaterThanSecond<A, B> & IsPositive extends never
@@ -28,10 +42,18 @@ type Comparator<A extends number, B extends number> =
       ? `${B}` extends `${infer BF}${infer BR}`
         ? AF extends '-'
           ? BF extends '-'
-            ? CompareNumsWithSameSign<`${AR}`, `${BR}`, false>
+            ? GetLetters<AR>['length'] extends GetLetters<BR>['length']
+              ? CompareNumsWithSameSign<`${AR}`, `${BR}`, false>
+              : FirstIsLongerThanSecond<AR, BR> extends true
+                ? Comparison.Lower
+                : Comparison.Greater
             : Comparison.Lower
           : BF extends '-'
             ? Comparison.Greater
-            : CompareNumsWithSameSign<`${A}`, `${B}`, true>
+            : GetLetters<`${A}`>['length'] extends GetLetters<`${B}`>['length']
+              ? CompareNumsWithSameSign<`${A}`, `${B}`, true>
+              : FirstIsLongerThanSecond<`${A}`, `${B}`> extends true
+                ? Comparison.Greater
+                : Comparison.Lower
         : never
       : never
